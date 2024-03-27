f6d03cb3e58b201bb5503460bd5bd8053f222d5f
sathaythan <sathyathangavel7@gmail.com>
2024-03-27 13:30:16 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //TASK 1:
 //CREATE RESUME FOR JSON FORMAT
-
+/*
 var resume={
     "name":"sathya",
     "email":"sathyathangavel7@gmail.com",
@@ -8,29 +8,79 @@ var resume={
     "degree":"BE",
     "department":"computer science",
     "languagesknown":"tamil,english",
-    "address":"1/2 kariyakaliamman kovil street",
+     "address":"1/2 kariyakaliamman kovil street",
     "postalcode":"641653",
     "city":"coimabtore",
     "state":"tamilnadu",
     "country":"india",
-
-}
-console.log(resume);
-//for loop
+       }
+       console.log(resume);
+*/
+//for loop:
+/*var resume=[{
+    "name":"sathya",
+    "email":"sathyathangavel7@gmail.com",
+    "phoneno":"9786361262",
+    "degree":"BE",
+    "department":"computer science",
+    "languagesknown":"tamil,english",
+     "address":"1/2 kariyakaliamman kovil street",
+    "postalcode":"641653",
+    "city":"coimabtore",
+    "state":"tamilnadu",
+    "country":"india",
+       }]
+       console.log(resume);
 for(var i=0;i<resume.length;i++)
-    {
-        console.log(resume[i]);
-    }
-//for in loop
-for(var i in resume)
 {
     console.log(resume[i]);
-}
-//for of loop
-for(var newresume of i)
+}*/
+
+//for in loop
+/*var resume={
+    "name":"sathya",
+    "email":"sathyathangavel7@gmail.com",
+    "phoneno":"9786361262",
+    "degree":"BE",
+    "department":"computer science",
+    "languagesknown":"tamil,english",
+     "address":"1/2 kariyakaliamman kovil street",
+    "postalcode":"641653",
+    "city":"coimabtore",
+    "state":"tamilnadu",
+    "country":"india",
+       }
+       console.log(resume);
+
+for(var i in resume)
 {
-    console.log(newresume);
+    console.log(i,resume[i]);
+
+}*/
+
+//for of loop
+
+const result23=[postal=641653,phoneno=9786361262];
+for(var newresult23 of result23)
+{
+    console.log(newresult23);
 }
+
 //for each loop
-resume.forEach((element => console.log(element)));
-    
+
+var result24=[641653,9786361262];
+result24.forEach(numbers=>console.log(numbers));
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
